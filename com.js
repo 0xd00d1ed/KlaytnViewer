a393c6fb4ed4058433beecff70a219da341a15b2
qkrrjs4277 <qkrrjs4277@naver.com>
2022-12-20 18:50:11 +0900

    Implements get event

--- com.js
+++ com.js
@@ -1,28 +1,33 @@
-var defaultRpcUrl = 'https://public-node-api.klaytnapi.com/v1/cypress';
+var defaultRpcUrl = "https://public-node-api.klaytnapi.com/v1/cypress";
 var contract;
 var caver;
 var presetList;
 
+// 블록체인 통신을 위한 Caver 라이브러리
 const setCaver = (rpcUrl) => {
     if (!rpcUrl) return;
-    return caver = new Caver(new Caver.providers.HttpProvider(rpcUrl));
-}
+    return (caver = new Caver(new Caver.providers.HttpProvider(rpcUrl)));
+};
 
+// 컨트랙트 주소와 ABI를 입력하여 컨트랙트 정보를 설정한다.
 const setContract = (address, abi) => {
     if (!address && !abi) return;
-    return contract = new caver.contract(JSON.parse(abi), Caver.utils.toChecksumAddress(address));
-}
+    return (contract = new caver.contract(JSON.parse(abi), Caver.utils.toChecksumAddress(address)));
+};
 
+// 정의된 프리셋을 불러온다.
 const setPreset = (preset) => {
     presetList = preset;
     return presetList;
-}
+};
 
+// 컨트랙트의 함수 정보를 불러온다.
 const getJsonInterface = () => {
     if (!contract) return;
     return contract._jsonInterface;
-}
+};
 
+// 함수를 소스코드가 아닌 문자열로 실행할 수 있는 기능
 const executeFunctionByName = async (functionName, context, args) => {
     let namespaces = functionName.split(".");
     let func = namespaces.pop();
@@ -30,4 +35,54 @@ const executeFunctionByName = async (functionName, context, args) => {
         context = context[namespaces[i]];
     }
     return await context[func].apply(context, args);
-}
+};
+
+// 설정된 컨트랙트의 이벤트를 불러온다. 블록 기간으로 조회가 가능한데,
+// 이벤트 데이터가 너무 많을 시 오류가 발생하므로 재귀적인 방법으로
+// 분할 정복하여 이벤트를 가져온다.
+const readEventsRange = (contract, event, options, callback) =>
+    new Promise((resolve, reject) => {
+        (async () => {
+            contract.getPastEvents(event, options, async (errors, events) => {
+                if (!errors) {
+                    console.log(
+                        "starts processing blocks",
+                        options.fromBlock,
+                        "..",
+                        options.toBlock
+                    );
+                    for (const event of events) {
+                        await callback(event);
+                    }
+                    console.log("done processing blocks", options.fromBlock, "..", options.toBlock);
+                    resolve();
+                } else {
+                    const middle = options.fromBlock.plus(options.toBlock).div(2).integerValue();
+                    let message =
+                        "Error [" +
+                        options.fromBlock +
+                        ".." +
+                        options.toBlock +
+                        "] " +
+                        "->  [" +
+                        options.fromBlock +
+                        ".." +
+                        middle +
+                        "] " +
+                        "and [" +
+                        (middle + 1) +
+                        ".." +
+                        options.toBlock +
+                        "]";
+                    console.log(message);
+                    const leftOpts = structuredClone(options);
+                    const rightOpts = structuredClone(options);
+                    leftOpts.toBlock = middle;
+                    await readEventsRange(contract, event, leftOpts, callback);
+                    rightOpts.fromBlock = middle + 1;
+                    await readEventsRange(contract, event, rightOpts, callback);
+                    resolve();
+                }
+            });
+        })();
+    });
---
+++ presets.js
@@ -0,0 +1,42 @@
+const clone = (obj) => {
+    return JSON.parse(JSON.stringify(obj));
+};
+const list = [];
+const template = {
+    category: undefined,
+    contract: [],
+};
+
+const standard = clone(template);
+standard.category = "표준";
+import standard_erc20 from "./preset/standard/erc20.js";
+standard.contract.push(standard_erc20);
+
+const klayswap = clone(template);
+klayswap.category = "클레이스왑";
+import klayswap_factory from "./preset/klayswap/factory.js";
+import klayswap_exchange from "./preset/klayswap/exchange.js";
+import klayswap_governor from "./preset/klayswap/governor.js";
+import klayswap_poolvoting from "./preset/klayswap/poolvoting.js";
+klayswap.contract.push(klayswap_factory);
+klayswap.contract.push(klayswap_exchange);
+klayswap.contract.push(klayswap_poolvoting);
+klayswap.contract.push(klayswap_governor);
+
+const sigma = clone(template);
+sigma.category = "시그마";
+import sigma_vote from "./preset/sigma/vote.js";
+import sigma_govern from "./preset/sigma/govern.js";
+sigma.contract.push(sigma_vote);
+sigma.contract.push(sigma_govern);
+
+const kokoa = clone(template);
+kokoa.category = "코코아 파이낸스";
+import psm from "./preset/kokoa/psm.js";
+kokoa.contract.push(psm);
+
+list.push(standard);
+list.push(klayswap);
+list.push(sigma);
+list.push(kokoa);
+export default list;
